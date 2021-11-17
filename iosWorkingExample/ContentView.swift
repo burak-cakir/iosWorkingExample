70d9612dba1aab45d5d29a34d475feab92e79e25
burak-cakir <burakcakir@hotmail.com.tr>
2021-11-17 19:11:33 +0300

    Initial Commit

---
+++ iosWorkingExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosWorkingExample
+//
+//  Created by burak cakir on 17.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosWorkingExample/iosWorkingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosWorkingExampleApp.swift
+//  iosWorkingExample
+//
+//  Created by burak cakir on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct iosWorkingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
