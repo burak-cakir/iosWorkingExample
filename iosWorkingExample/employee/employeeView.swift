0757ba170658ee34da43e9a52f9356cfacf18b0c
burak-cakir <burakcakir@hotmail.com.tr>
2021-11-24 14:17:27 +0300

    mm

---
+++ iosWorkingExample/employee/employeeView.swift
@@ -0,0 +1,20 @@
+//
+//  employeeView.swift
+//  iosWorkingExample
+//
+//  Created by burak cakir on 24.11.2021.
+//
+
+import SwiftUI
+
+struct employeeView: View {
+    var body: some View {
+        Text("employee view")
+    }
+}
+
+struct employeeView_Previews: PreviewProvider {
+    static var previews: some View {
+        employeeView()
+    }
+}
